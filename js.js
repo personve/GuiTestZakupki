5288cc25bf681cb60bdc8cca361c0338dc932371
personve <vakara143@gmail.com>
2020-03-02 21:54:16 +0300

    commit

--- js.js
+++ js.js
@@ -5,7 +5,8 @@ function checkData(event) {
       if (document.forms.form2.pass.value === "123") {
         fio_tab();
         document.getElementById("hidePass").style.display = "none";
-        document.getElementById("hideQuestions").style.display = "block";
+        document.getElementById("questions").style.display = "block";
+        document.getElementById("progressBar").style.display = "block";
         progress();
       } else {
         alert("Неверный пароль-подтверждение");
@@ -16,7 +17,7 @@ function checkData(event) {
   } else {
     alert("Неверный табельный");
   }
-}
+} /* Валидация для начала теста */
 
 function fio_tab() {
   let val1 = document.getElementById("name").value;
@@ -24,33 +25,31 @@ function fio_tab() {
   document.getElementById("hideHello").style.display = "block";
   fio.innerHTML = val1;
   tab.innerHTML = val2;
-}
+} /* Заполнение поля приветствия тестируемого */
 
 function progress() {
   var width = 100;
   var elem = document.getElementById("progress_line");
-  var time1 = Math.round((300 * 1000) / 100);
+  var time1 = Math.round((500 * 1000) / 100);
   var id = setInterval(progressStatus, time1);
 
   function progressStatus() {
     if (width == 0) {
       clearInterval(id);
-      document.getElementById("hideQuestions").style.display = "none";
+      document.getElementById("questions").style.display = "none";
+      document.getElementById("progressBar").style.display = "none";
       document.getElementById("result").style.display = "block";
+      res.innerHTML = 16; /* Результат теста */
     } else {
       width--;
       elem.style.width = width + "%";
-      getMass();
     }
   }
-}
+} /* Основной код теста */
 
-function getMass() {
-  var mass = [null];
-  rightBut1.onclick = function () {
-    mass.push(20);
-    alert(mass);
-  }
+function getBall() {
+  var ball = 0;
+  rightBut1.onclick = function() {};
 }
 
 function getSumm() {
@@ -61,4 +60,3 @@ function getSumm() {
   return sumRes;
 }
 
-function res() {}
